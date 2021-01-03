32bd94af57a73adb9c08740eda035a63e9c412eb
T-unity <takadanaritoshi@gmail.com>
2021-01-03 23:23:00 +0900

    雛形

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import About from './components/About'
+import Skills from './components/Skills'
+import Works from './components/Works'
 
 const App: React.FC = () => {
   return (
     <>
-      <Navbar />
-      <About />
+
+      <section>
+        <Navbar />
+      </section>
+
+      <section id='about'>
+        <About />
+      </section>
+
+      <section id='skills'>
+        <Skills />
+      </section>
+
+      <section id='works'>
+        <Works />
+      </section>
+
     </>
   );
 }
